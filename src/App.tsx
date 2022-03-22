b91dfc76997ca6aea824a8490fb73c9193855cdf
VedantS-UD <vsub@udel.edu>
2022-03-22 15:17:21 -0400

    Testing Welcome Message and Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript (Team 10)
             </header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
+                Edit <code>src/App.tsx</code> and save. This page will3
                 automatically reload.
             </p>
         </div>
