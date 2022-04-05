a66da902cd3a6c9cb39e14a090fe8fd2945edc3e
Evelyn <ejw2900@gmail.com>
2022-04-05 15:05:02 -0400

    Trying again

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript (Team 10) Evelyn
-                Welsh
+                UD CISC275 with React Hooks and TypeScript Evelyn Welsh(Team 10)
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will3
